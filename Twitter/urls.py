c928cf4424dad0acc3517039d78643d209cfbf70
Sam Lee <smlee18@sas.upenn.edu>
2020-11-28 15:36:34 -0500

    test page

--- Twitter/urls.py
+++ Twitter/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from twitterApp.views import test
 
 # links URL to view
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', test, name="splash")
 ]
